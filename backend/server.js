c82afaec90060dac12f2fb3d808e8dae1f6fea6a
RahanBenabid <rahannadime@gmail.com>
2024-11-27 16:22:32 +0100

    Cors implementation to handle many ports

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+app.use(
+	cors({
+		origin: 'http://localhost:5500',
+	})
+);
 
 import weatherRoutes from "./routes/WeatherRoutes.js";
 
